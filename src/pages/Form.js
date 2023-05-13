6ef47a196468a626cb7f346f9792e4ecf335482e
samhitha16 <p.samhitharao22@gmail.com>
2023-05-13 11:36:18 +0530

    adding form

---
+++ src/pages/Form.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export const Form = ({ onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
+        <label htmlFor="name">Project Title</label>
+        <input className="form-control" id="name" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          placeholder="name@example.com"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="description">Project Description</label>
+        <input
+          type="text"
+          className="form-control"
+          id="description"
+        //   placeholder="name@example.com"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="Required amount">Required amount</label>
+        <input
+          type="currency"
+          className="form-control"
+          id="Required amount"
+          placeholder="xyz/-"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="Payment details">Payment details</label>
+        <input
+          type="currency"
+          className="form-control"
+          id="Payment details"
+          placeholder="UPI ID"
+        />
+      </div>
+      <div className="form-group">
+        <button className="form-control btn btn-primary" type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+export default Form;
--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -1,7 +1,11 @@
 import { Button } from '@mui/material'
 import { useContext } from 'react';
 import { UserContext } from '../contexts/user.context';
- 
+import ReactModal from 'react-modal'; 
+import React, {useState} from 'react';
+
+import { Container } from './container';
+
 export default function Home() {
  const { logOutUser } = useContext(UserContext);
  
@@ -18,12 +22,79 @@ export default function Home() {
    } catch (error) {
      alert(error)
    }
- }
- 
+}
+
+const [userInput,setUserInput] = useState('')
+
+const handleChange =(event)=>{
+    setUserInput(event.target.value)
+}
+
+const [isOpen, setIsOpen] = useState(false);
+
+// const form = async () => {
+//     // alert("Great");}
+
+const triggerText = 'Open form';
+const onSubmit = (event) => {
+  event.preventDefault(event);
+  console.log(event.target.name.value);
+  console.log(event.target.email.value);
+};
+
  return (
    <>
      <h1>Welcome to Expengo</h1>
+     
+     <div className="Home">
+      <Container triggerText={triggerText} onSubmit={onSubmit} />
+    </div>
+    
+     {/* <div>
+      <button variant="contained" onClick={() => setIsOpen(true)}>Create project</button>
+      <ReactModal
+        isOpen={isOpen}
+        contentLabel="Example Modal"
+        onRequestClose={() => setIsOpen(false)}
+      >Fill the details about your project:
+    <form>
+        <div className="form-input">
+            <input 
+                type="text"
+                name="title"
+                placeholder="Enter your title"
+                value={userInput}
+                // value=""
+                onChange={handleChange}
+            />
+        </div>
+        <div className="form-input">
+             <textarea 
+                placeholder="Enter body" 
+                name="body" 
+                cols="30" 
+                rows="10" 
+                // value="" 
+                value={userInput}
+                onChange={handleChange}
+            ></textarea>
+        </div>
+         <button>Submit</button>
+
+    </form>
+        
+      </ReactModal>
+    </div> */}
+     <h2> Hello world</h2>
      <Button variant="contained" onClick={logOut}>Logout</Button>
    </>
- )
+ );
+
+
+//    return(
+//     <>
+//     <Button onClick={form}>Create Project</Button>
+//     </>
+//    );
 }
+
